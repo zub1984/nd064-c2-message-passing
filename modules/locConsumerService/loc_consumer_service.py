d0c61128780a2e0295a97430ef5e53ad59c83d59
Jubair <zub1984.kn@gmail.com>
2021-10-18 23:36:55 +0300

    kafka topic test

--- modules/locConsumerService/loc_consumer_service.py
+++ modules/locConsumerService/loc_consumer_service.py
@@ -12,9 +12,10 @@ DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
 
+logging.info('kafka_url : ', kafka_url)
+logging.info('kafka_topic : ', kafka_topic)
+
 consumer = KafkaConsumer(kafka_topic, bootstrap_servers=[kafka_url])
-logging.debug('kafka_url : ', kafka_url)
-logging.debug('kafka_topic : ', kafka_topic)
 
 def write_to_db(location):
     conn = create_engine(f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
